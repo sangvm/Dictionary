4d6f8364ce5a42d5b82f86e8dbf352ed0fb5f9e8
Vũ Minh Sang <80307614+sangvm@users.noreply.github.com>
2023-03-19 17:01:08 +0700

    Update

---
+++ app/src/main/java/com/example/dictionary/ui/Dictionary/DictionaryFragment.java
@@ -0,0 +1,38 @@
+package com.example.dictionary.ui.Dictionary;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dictionary.R;
+
+public class DictionaryFragment extends Fragment {
+
+    private DictionaryViewModel mViewModel;
+
+    public static DictionaryFragment newInstance() {
+        return new DictionaryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dictionary, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DictionaryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/dictionary/ui/Dictionary/DictionaryViewModel.java
@@ -0,0 +1,7 @@
+package com.example.dictionary.ui.Dictionary;
+
+import androidx.lifecycle.ViewModel;
+
+public class DictionaryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
