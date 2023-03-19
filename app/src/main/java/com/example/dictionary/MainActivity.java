08cc0288b44d5639aae8a3acfd0276dea6a2ca30
Vũ Minh Sang <80307614+sangvm@users.noreply.github.com>
2023-03-19 15:46:44 +0700

    Update

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.dictionary;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.fragment.app.Fragment;
@@ -37,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.homeFragment)
+                R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -63,6 +61,6 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        fragmentTransaction.add(R.id.
+        //fragmentTransaction.add(R.id.
     }
 }
---
+++ app/src/main/java/com/example/dictionary/ui/bookmark/BookmarkViewModel.java
@@ -0,0 +1,7 @@
+package com.example.dictionary.ui.bookmark;
+
+import androidx.lifecycle.ViewModel;
+
+public class BookmarkViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
