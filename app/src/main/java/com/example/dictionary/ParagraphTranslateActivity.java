321022909b86e63647cfb809fbb1363ef6ef2c50
Vũ Minh Sang <80307614+sangvm@users.noreply.github.com>
2023-05-05 16:29:40 +0700

    Dịch văn bản

--- app/src/main/java/com/example/dictionary/ParagraphTranslateActivity.java
+++ app/src/main/java/com/example/dictionary/ParagraphTranslateActivity.java
@@ -1,25 +1,211 @@
 package com.example.dictionary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.mlkit.common.model.DownloadConditions;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class ParagraphTranslateActivity extends AppCompatActivity {
 
-    private EditText firstTextbox;
-    private EditText secondTextbox;
-    private Button translateButton;
+    private static final String TAG = "MAIN_TAG";
 
+    private EditText sourceLanguageEditText;
+    private TextView destinationLanguageTextView;
+    private MaterialButton translateButton;
+    private MaterialButton sourceLanguageChooseButton;
+    private MaterialButton destinationLanguageChooseButton;
+
+    private ArrayList<ModelLanguage> languageArrayList;
+
+    private TranslatorOptions translatorOptions;
+    private Translator translator;
+    private ProgressDialog progressDialog;
+
+    private String sourceLanguageCode = "en";
+    private String sourceLanguageTitle = "English";
+
+    private String destinationLanguageCode = "vi";
+    private String destinationLanguageTitle = "Vietnamese";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paragraph_translate);
 
-        firstTextbox = findViewById(R.id.first_textbox);
-        translateButton = findViewById(R.id.translate_button);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
 
+        loadAvailableLanguages();
+
+        sourceLanguageEditText = findViewById(R.id.sourceLanguage);
+        destinationLanguageTextView = findViewById(R.id.destinationLanguage);
+        sourceLanguageChooseButton = findViewById(R.id.sourceLanguageChooseBtn);
+        destinationLanguageChooseButton = findViewById(R.id.destinationLanguageChooseBtn);
+        translateButton = findViewById(R.id.translateBtn);
+
+        sourceLanguageChooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sourceLanguageChoose();
+            }
+        });
+        destinationLanguageChooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                destinationLanguageChoose();
+            }
+        });
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+            }
+        });
     }
 
+
+    private String sourceLanguageText = "";
+    private void validateData() {
+        sourceLanguageText = sourceLanguageEditText.getText().toString().trim();
+        Log.d(TAG, "validateData: " + sourceLanguageText);
+        if (sourceLanguageText.isEmpty()) {
+            Toast.makeText(this, "Enter text to translate", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            startTranslation();
+        }
+    }
+
+    private void startTranslation() {
+        progressDialog.setMessage("Processing language model...");
+        progressDialog.show();
+        translatorOptions = new TranslatorOptions.Builder()
+                .setSourceLanguage(sourceLanguageCode)
+                .setTargetLanguage(destinationLanguageCode)
+                .build();
+        translator = Translation.getClient(translatorOptions);
+
+        DownloadConditions downloadConditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+
+        translator.downloadModelIfNeeded(downloadConditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "onSuccess: model ready, start translate");
+
+                        progressDialog.setMessage("Translating...");
+
+                        translator.translate(sourceLanguageText)
+                                .addOnSuccessListener(new OnSuccessListener<String>() {
+                                    @Override
+                                    public void onSuccess(String translatedText) {
+                                        Log.d(TAG, "onSuccess: translatedText " + translatedText);
+                                        progressDialog.dismiss();
+                                        destinationLanguageTextView.setText(translatedText);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        progressDialog.dismiss();
+                                        Toast.makeText(ParagraphTranslateActivity.this, "Failed to translate due to " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Log.d(TAG, "onFailure: ", e);
+                        Toast.makeText(ParagraphTranslateActivity.this, "Failed to ready model due to " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void loadAvailableLanguages() {
+        languageArrayList = new ArrayList<>();
+        List<String> languageCodeList = TranslateLanguage.getAllLanguages();
+        for (String languageCode : languageCodeList) {
+            String languageTitle = new Locale(languageCode).getDisplayLanguage();
+            Log.d(TAG, "Load Available Language: " + languageCode);
+
+            ModelLanguage modelLanguage = new ModelLanguage(languageCode, languageTitle);
+            languageArrayList.add((modelLanguage));
+        }
+    }
+
+    private void sourceLanguageChoose() {
+        PopupMenu popupMenu = new PopupMenu(this, sourceLanguageChooseButton);
+        for (int i = 0; i < languageArrayList.size(); i++) {
+            popupMenu.getMenu().add(Menu.NONE, i, i, languageArrayList.get(i).languageTitle);
+        }
+        popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int position = menuItem.getItemId();
+
+                sourceLanguageCode = languageArrayList.get(position).languageCode;
+                sourceLanguageTitle = languageArrayList.get(position).languageTitle;
+
+                sourceLanguageChooseButton.setText(sourceLanguageTitle);
+
+//                sourceLanguageEditText.setHint("Nhập");
+
+                Log.d(TAG, "onMenuItemClick: sourceLanguageCode: " + sourceLanguageCode);
+                return false;
+            }
+        });
+    }
+
+    private void destinationLanguageChoose() {
+        PopupMenu popupMenu = new PopupMenu(this, destinationLanguageChooseButton);
+        for (int i = 0; i < languageArrayList.size(); i++) {
+            popupMenu.getMenu().add(Menu.NONE, i, i, languageArrayList.get(i).languageTitle);
+        }
+        popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int position = menuItem.getItemId();
+
+                destinationLanguageCode = languageArrayList.get(position).languageCode;
+                destinationLanguageTitle = languageArrayList.get(position).languageTitle;
+
+                destinationLanguageChooseButton.setText(destinationLanguageTitle);
+
+
+                Log.d(TAG, "onMenuItemClick: destinationLanguageCode: " + destinationLanguageCode);
+                return false;
+            }
+        });
+    }
 }
